a07342fcb6bf4da4fd5645a2ce5a0da2931db4a9
canxue <1419761729@qq.com>
2020-04-20 19:07:13 +0800

    购物车练习

--- vue/03/书籍购物车/main.js
+++ vue/03/书籍购物车/main.js
@@ -32,8 +32,32 @@ const app = new Vue({
       for (let i = 0; i < this.books.length; i++) {
         totalPrice += this.books[i].count * this.books[i].price;
       }
-      return totalPrice;
+      // return totalPrice;
+
+
+      return this.books.reduce((book,n)=>book.count*book.price);
+    },
+
+
+
+
+    //  reduce的使用, 对数组里的所有内容进行汇总
+
+    total() {
+      let newNum = [100, 200, 344, 5666];
+      // 参数1是一个方法preValue是拿到上一次的返回值,value
+      // 是这次的循环值, 所以可以用这个来进行累加操作, 其中第二个的值是第一次的prevalue的默认值.
+      newNum.reduce(function (preValue, value) {
+        return preValue + value;
+      }, 0);
+
+      //filter , map , reduce的高阶函数.
+      // 是使用lanmda的剪头函数来进行替代function
+      newNum.filter(n => n < 100).map(n => n * 2).reduce((pre, n) => pre + n);
+
+
     }
+
   },
   //过滤器,
   filters: {
